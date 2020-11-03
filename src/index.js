4a954f46747c448dbb9b790cd3d30d6bdc1dbf27
camelFace7122 <sanity.7122@gmail.com>
2020-11-03 22:27:17 +0500

    fixed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
